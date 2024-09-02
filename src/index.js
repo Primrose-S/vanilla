0310def42c1120df724cc920bc5f5eab8d5151e4
Primrose-S <primrosedube71@gmail.com>
2024-09-02 18:31:55 +0200

    improved my forecast api

--- src/index.js
+++ src/index.js
@@ -16,7 +16,8 @@ function refreshWeather(response) {
   windSpeedElement.innerHTML = `${response.data.wind.speed}km/h`;
   temperatureElement.innerHTML = Math.round(temperature);
   iconElement.innerHTML = `<img src="${response.data.condition.icon_url}" class="weather-app-icon" />`;
-  getForecast ("Paris");
+
+  getForecast(response.data.city);
 }
 
 function formatDate(date) {
@@ -42,7 +43,7 @@ function formatDate(date) {
 
 function searchCity(city) {
   let apiKey = "b2a5adcct04b33178913oc335f405433";
-  let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}`;
+  let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric`;
   axios.get(apiUrl).then(refreshWeather);
 }
 
@@ -52,6 +53,7 @@ function handleSearchSubmit(event) {
 
   searchCity(searchInput.value);
 }
+
 function formatDay(timestamp) {
   let date = new Date(timestamp * 1000);
   let days = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
